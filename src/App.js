8ee248db101a91db80183842be84513dfa603d96
IrynaVlasenko <106557356+IrynaVlasenko@users.noreply.github.com>
2022-10-03 20:27:22 +0300

    Added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <h2>TEXAS</h2>
+            <h1>
+              <span></span>
+              <span className="degrees">
+                <a href="/" className="active">
+                  19°C
+                </a>
+                |<a href="/">33°F</a>
+              </span>
+              <br />
+              <img
+                src="https://cdn.icon-icons.com/icons2/2505/PNG/512/sun_weather_icon_150657.png"
+                alt=""
+              ></img>
+            </h1>
+
+            <form>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search"
+                autoComplete="off"
+              />
+              <button type="button" className="btn btn-primary">
+                Search
+              </button>
+            </form>
+
+            <p>
+              Last updated:
+              <span>Saturday</span>
+            </p>
+            <p>Snowing</p>
+            <p>
+              Humidity <span>77</span>%{" "}
+            </p>
+            <p>
+              Wind <span>30</span>km/h{" "}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
